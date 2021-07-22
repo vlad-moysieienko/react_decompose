9a5ff852ae9847a7b98657e8061410577eaa2b21
Vlad Moysieienko <Moysieienko03@gmail.com>
2021-07-22 13:19:07 +0300

    Updates

---
+++ precache-manifest.9392600c2714fbdcfd0d9e5e07594040.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68388ed3d8172e52dd9e40e3faf31c56",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bc7364d4b09043129c14",
+    "url": "./static/css/main.1fc450e5.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc7364d4b09043129c14",
+    "url": "./static/js/main.d5315056.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
